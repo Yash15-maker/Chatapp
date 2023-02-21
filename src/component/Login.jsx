34f8299616246b26d59e02724aad275b367c6b4c
Yash Mishra <64361746+Yash15-maker@users.noreply.github.com>
2023-02-21 14:41:41 +0530

    Update Login.jsx

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { auth } from "../Firebase";
 import firebase from "firebase/compat/app";
-// import GoogleAuthProvider from "firebase/auth";
-import './css/Button.css'
+import "./css/Button.css";
 function Login() {
   function handleSignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
   return (
-    <div className="div-button">
-      <button className="button" onClick={handleSignIn}>Sign In With Google</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <button className="button" onClick={handleSignIn}>
+        Sign In With Google
+      </button>
     </div>
   );
 }
